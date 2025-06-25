7982e2b17110192edffa04ec7a27fb4671f79c81
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-06-25 11:21:18 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -9,8 +9,8 @@ import httpx
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from aiofiles.os import path as aiopath
 
-from src.helpers import CachedTrack
-from src.logger import LOGGER
+from AviaxMusic.helpers import CachedTrack
+from AviaxMusic.logger import LOGGER
 
 FONTS = {
     "cfont": ImageFont.truetype("src/modules/utils/cfont.ttf", 15),
