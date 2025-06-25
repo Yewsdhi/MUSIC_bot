7d7a98c9197fb5393df0eda0cb77956e714bfd9f
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-06-25 11:36:42 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -1,6 +1,5 @@
-#  Copyright (c) 2025 AshokShau
-#  Licensed under the GNU AGPL v3.0: https://www.gnu.org/licenses/agpl-3.0.html
-#  Part of the TgMusicBot project. All rights reserved where applicable.
+# ATLEAST GIVE CREDITS IF YOU STEALING :(((((((((((((((((((((((((((((((((((((
+# ELSE NO FURTHER PUBLIC THUMBNAIL UPDATES
 
 import asyncio
 from io import BytesIO
@@ -9,8 +8,8 @@ import httpx
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from aiofiles.os import path as aiopath
 
-from src.helpers import CachedTrack
-from src.logger import LOGGER
+from AviaxMusic.helpers import CachedTrack
+from AviaxMusic.logger import LOGGER
 
 FONTS = {
     "cfont": ImageFont.truetype("src/modules/utils/cfont.ttf", 15),
